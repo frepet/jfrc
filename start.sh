22c3438463319b0a7ddc93d61cb10b0a4526b67e
Johan Peteri <peterijohan@gmail.com>
2021-06-04 22:38:05 +0200

    Adds venv to start script

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+printf "Activating venv\n"
+. venv/bin/activate
+
 printf "Starting ServoBlaster\n"
 sudo servod --min=500us --max=1000us --cycle-time=10000us
 
@@ -10,3 +13,6 @@ python jfrc_video_server.py & python jfrc_server.py
 
 printf "Stopping ServoBlaster\n"
 sudo killall servod
+
+printf "Deactivating venv\n"
+deactivate
